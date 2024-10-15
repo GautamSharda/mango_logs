a0614aeb47803ea7a44989a5903381e753cee9b0
GautamSharda <57648023+GautamSharda@users.noreply.github.com>
2024-10-15 18:56:25 -0500

    server

---
+++ logging_server.py
@@ -0,0 +1,17 @@
+from flask import Flask, request
+
+app = Flask(__name__)
+
+@app.route('/log', methods=['POST'])
+def log():
+    message = request.get_data(as_text=True)
+    print({message})
+    return "Log received", 200
+
+@app.route('/test', methods=['GET'])
+def test():
+    return "Test successful", 200
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
+
